68c59bbeed8f5761d24cbd850e85b0cf10fdc5db
Chiflado <prjevarabalazs@gmail.com>
2022-12-05 18:00:10 +0100

    Adds lottery logic and basic template

--- lottery-machine/src/app/app.module.ts
+++ lottery-machine/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { MainComponent } from './main/main.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
--- lottery-machine/src/app/main/main.component.ts
+++ lottery-machine/src/app/main/main.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
-
+  numberPool: number[] = [];
+  winningNumbers: number[] = [];
+  
+  constructor() {}
+  
   ngOnInit(): void {
+    
   }
 
+  start(): void {
+    this.resetNumbers();
+    for (let i = 0; i < 5; i++) {
+      const numberIndex = this.getRandomInt(0, this.numberPool.length);
+      this.winningNumbers.push(this.numberPool[numberIndex]);
+      this.numberPool.splice(numberIndex, 1);
+    }
+    console.log(this.winningNumbers, this.numberPool);
+  } 
+
+  getRandomInt(min: number, max: number): number {       
+    var byteArray = new Uint8Array(1);
+    window.crypto.getRandomValues(byteArray);
+
+    var range = max - min + 1;
+    var max_range = 256;
+    if (byteArray[0] >= Math.floor(max_range / range) * range)
+        return this.getRandomInt(min, max);
+    return min + (byteArray[0] % range);
+  }
+
+  resetNumbers(): void {
+    this.winningNumbers = [];
+    for (let i = 1; i < 91; i++) {
+      this.numberPool.push(i);
+    }
+    console.log(this.numberPool);
+  }
 }
